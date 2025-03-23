c406d8e9739d536a3f061da13f05da23f86a2662
wrapjss <103901449+wrapjss@users.noreply.github.com>
2025-03-23 07:45:37 -0400

    Add intl/i18n config import from i18n.ts

--- app/source.ts
+++ app/source.ts
@@ -1,9 +1,11 @@
 import { docs, meta } from "@/.source";
+import { i18n } from '@/i18n';
 import { loader } from "fumadocs-core/source";
 import { createMDXSource } from "fumadocs-mdx";
 import { icons } from "lucide-react";
 import { create } from "./create-icon";
 export const source = loader({
+	i18n,
 	baseUrl: "/docs",
 	source: createMDXSource(docs, meta),
 	icon(icon) {
